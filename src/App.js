c37ec9103430f464272ed47ba5477cb4591b39d4
Nischal Basnet <nischallukla@gmail.com>
2024-02-22 10:13:01 +0545

    updated latest

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './app.css';
 import About from './Components/About/About'
 import Blog from './Components/Blog/Blog'
 import Footer from './Components/Footer/Footer'
